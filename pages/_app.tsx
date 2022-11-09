2365e23d6b35b5d766205beecd54841404a4585c
Okute Wonah <okutewonah@gmail.com>
2022-11-09 19:33:12 +0200

    added sass, to solve error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/main.css'
+import '../styles/main.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Page from '../layouts/Pages'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Page>
-      <main className={styles.main}>
+      <main>
         
       </main>
     </Page>
