d1c57a259b99d7209c0e444f23f4aa1295482ce9
Okute Wonah <okutewonah@gmail.com>
2022-11-16 23:09:08 +0200

    bg-dark title positioning

--- components/BgDark.tsx
+++ components/BgDark.tsx
@@ -5,7 +5,13 @@ interface Props {}
 const BgDark: React.FC<Props> = () => {
   return (
     <div className="bg-dark">
-      <img src="/assets/images/bg-desktop-dark.jpg" alt="background-dark" />
+      <img className="bg-dark__img" src="/assets/images/bg-desktop-dark.jpg" alt="background-dark" />
+      <div className="bg-dark__container">
+        <div className="bg-dark__todo-top">
+          <span className="bg-dark__title">TODO</span>
+          <img src="/assets/images/icon-sun.svg" alt="sun icon" />
+        </div>
+      </div>
     </div>
   )
 }
